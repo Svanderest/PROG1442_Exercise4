4869334b641345a30aeb1d1dd8d60aa72b114e55
Niagara College <helpdesk@niagaracollege.ca>
2019-03-12 15:34:57 -0400

    finished api

--- PROG1442_Exercise4/Models/ArtType.cs
+++ PROG1442_Exercise4/Models/ArtType.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace PROG1442_Exercise4.Models
 {
+    [DataContract(IsReference = true)]
     public class ArtType
     {
         public ArtType()
         {
             this.Artworks = new HashSet<Artwork>();
         }
+
+        [DataMember]
         public int ID { get; set; }
 
+        [DataMember]
         [Display(Name = "Art Type")]
         [Required(ErrorMessage = "You cannot leave the art type name black.")]
         [StringLength(25, ErrorMessage = "Art type cannot be more than 25 characters long.")]
